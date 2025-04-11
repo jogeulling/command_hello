0cd41f77b165a9a53f3e06c26b2e428e87d6ec31
jogeulling <147814726+jogeulling@users.noreply.github.com>
2025-04-11 09:54:08 +0900

    github for git pull

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 // for command line git tutorial
 // local repository modification
-
+// github modification
+  
 print("Hello World")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
